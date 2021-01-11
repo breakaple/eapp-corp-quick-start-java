de7be0da7616c4fef76a1b8f2dedb0a2e7097dd9
MrQiucong <posuiapple@126.com>
2021-01-11 19:02:02 +0800

    change appkey

--- src/main/java/com/config/Constant.java
+++ src/main/java/com/config/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
     /**
      * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppKey
      */
-    public static final String APP_KEY = "***";
+    public static final String APP_KEY = "dingnmnzvk4mnnoqahvl";
     /**
      * 开发者后台->应用开发-企业内部应用->选择您创建的小程序->应用首页-查看详情->查看AppSecret
      */
-    public static final String APP_SECRET="***";
+    public static final String APP_SECRET="uyvMPH_5poF1Jt8APqjG_OWyHxASwK-MMobXCs4aBb-TiAj1tpIhxzO7KnxsAQyK";
 }
